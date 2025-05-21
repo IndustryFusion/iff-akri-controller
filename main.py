d0b8032f7b880120084b8b487e3bb31c4bb98bb4
Abhijith H R <abhijith.hr@ib-systems.org>
2025-05-21 10:59:08 +0200

    Akri independent Gateway ID based onboarding
    1. CR creation on factory mongo insert
    2. Data and agent pod deployment on mongoinsert CR (based on new CRD)
    3. protocol independent onboarding (data service image can be replaced and the config is mounted from factory mongo)
    4. Using mongo stream for insert capture

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ import logging
 import json
 import os
 import yaml
-import kubernetes
+import kubernetes 
 import time
 import subprocess
 from resources.scripts.util import get_onboarding_token
@@ -30,7 +30,8 @@ from pymongo import MongoClient
 logging.basicConfig(level=logging.INFO)
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
-
+kubernetes.config.load_incluster_config()  # or load_kube_config() for local dev
+k8s = kubernetes.client.CustomObjectsApi()
 
 
 resources = './resources'
@@ -45,43 +46,57 @@ resources = './resources'
 # token = os.environ.get("GITHUB_TOKEN")
 mongoUrl = os.environ.get("FACTORY_MONGO_URL")
 mongoDbName = os.environ.get("FACTORY_MONGO_DB_NAME")
-
+deviceId = os.environ.get("DEVICE_IFRIC_ID")
+# Replace with your MongoDB connection string
+client = MongoClient(mongoUrl)
 # headers = {'Authorization': 'Bearer ' + token, "Accept": "application/vnd.github+json" }
 
 # # GitHub API URL for listing repository contents
 # contents_url = f'https://api.github.com/repos/{owner}/{repo}/contents/{path}'
 
+# Watch specific collection for insert events
+pipeline = [{'$match': {'operationType': 'insert'}}]
+collection = client[mongoDbName]["onboardings"]
 
-@kopf.on.create('Instance')
-@kopf.on.resume('Instance')
+with collection.watch(pipeline) as stream:
+    for change in stream:
+        doc = change["fullDocument"]
+        print("New document inserted:", doc)
+
+        if doc.get("gateway_id") == deviceId:
+            print("Match found, creating CR...")
+            k8s.create_namespaced_custom_object(
+                group="myorg.io",
+                version="v1",
+                namespace="default",
+                plural="mongoinserts",
+                body={
+                    "apiVersion": "myorg.io/v1",
+                    "kind": "MongoInsert",
+                    "metadata": {
+                        "generateName": "mongo-insert-"
+                    },
+                    "spec": {
+                        "db": "your_db",
+                        "collection": "your_collection",
+                        "document": doc
+                    }
+                }
+            )
+        else:
+            print("env mismatch, ignoring.")
+
+@kopf.on.create('myorg.io', 'v1', 'mongoinserts')
 def create_fn_pod(spec, name, namespace, logger, **kwargs):
     time.sleep(1)
 
-    # Replace with your MongoDB connection string
-    client = MongoClient(mongoUrl)
-
-    # Select the database and collection
-    db = client[mongoDbName]  # Replace with your database name
-    collection = db["onboardings"]  # Replace with your collection name
-
-    # Fetch all documents
-    documents = list(collection.find({}, {"_id": 0}))
-
     if namespace == "devices":
-        if 'opc' in name:
-            ip_from_akri = spec.get('brokerProperties').get('OPCUA_DISCOVERY_URL')
-        elif 'mqtt' in name:
-            ip_from_akri = spec.get('brokerProperties').get('MQTT_BROKER_URI')
-            main_mqtt_topic = spec.get('brokerProperties').get('MQTT_TOPIC')
-
-        ip_from_akri = str(ip_from_akri).split('//')[1].split(':')[0]
+        documents = list(collection.find({"gateway_id": deviceId}, {"_id": 0}))
         PLACEHOLDER = 'akri.sh/' + name
         
         # Make a request to get the repository contents
         # response = requests.get(contents_url, headers=headers)
         # response.raise_for_status()  # Raise an error if the request failed
-
-        logger.info(f"IP from Akri: {ip_from_akri}")
 
         # kopf.info("Initial response from GitHub GW config contents" + str(response), reason='SomeReason')
         # kopf.info("IP from Akri" + str(ip_from_akri), reason='SomeReason')
@@ -103,100 +118,93 @@ def create_fn_pod(spec, name, namespace, logger, **kwargs):
                 # kopf.info("Config Data from GitHub" + str(config_data), reason='SomeReason')
                 # kopf.info("IP address in Config from GitHub" + str(config_data['ip_address']), reason='SomeReason')
 
-                if str(config_data['ip_address']) == str(ip_from_akri):
-                    if config_data['protocol'] == 'opc-ua' or (config_data['protocol'] == 'mqtt' and config_data['main_topic'] == main_mqtt_topic):
+                config_map = kubernetes.client.V1ConfigMap(
+                    api_version="v1",
+                    kind="ConfigMap",
+                    metadata=kubernetes.client.V1ObjectMeta(name=config_data['pod_name'] + "-app-config"),
+                    data={"config.yaml": str(config_data['app_config'])}
+                )
 
-                        config_map = kubernetes.client.V1ConfigMap(
-                            api_version="v1",
-                            kind="ConfigMap",
-                            metadata=kubernetes.client.V1ObjectMeta(name=config_data['pod_name'] + "-app-config"),
-                            data={"config.yaml": str(config_data['app_config'])}
-                        )
+                kopf.adopt(config_map)
 
-                        kopf.adopt(config_map)
+                api = kubernetes.client.CoreV1Api()
+                obj = api.create_namespaced_config_map(
+                    namespace=namespace,
+                    body=config_map
+                )
 
-                        api = kubernetes.client.CoreV1Api()
-                        obj = api.create_namespaced_config_map(
-                            namespace=namespace,
-                            body=config_map
-                        )
+                logger.info(f"App config created successfully: {obj}")
 
-                        logger.info(f"App config created successfully: {obj}")
+                with open(resources + '/devices-config.yaml', 'r') as edit_file:
+                    edit_data = edit_file.read()
+                    formatted_yaml = edit_data.replace('pdt_mqtt_hostname', config_data['pdt_mqtt_hostname']).replace(
+                        'pdt_mqtt_port', str(config_data['pdt_mqtt_port'])).replace('secure_config', str(config_data['secure_config']).lower()).replace('pod_name', config_data['pod_name'])
+                    
+                    body_data=yaml.safe_load(formatted_yaml)
+                    kopf.adopt(body_data)
 
-                        with open(resources + '/devices-config.yaml', 'r') as edit_file:
-                            edit_data = edit_file.read()
-                            formatted_yaml = edit_data.replace('pdt_mqtt_hostname', config_data['pdt_mqtt_hostname']).replace(
-                                'pdt_mqtt_port', str(config_data['pdt_mqtt_port'])).replace('secure_config', str(config_data['secure_config']).lower()).replace('pod_name', config_data['pod_name'])
-                            
-                            body_data=yaml.safe_load(formatted_yaml)
-                            kopf.adopt(body_data)
+                    api = kubernetes.client.CoreV1Api()
+                    obj = api.create_namespaced_config_map(
+                        namespace=namespace,
+                        body=body_data
+                    )
 
-                            api = kubernetes.client.CoreV1Api()
-                            obj = api.create_namespaced_config_map(
-                                namespace=namespace,
-                                body=body_data
-                            )
-
-                            logger.info(f"Global devices config created successfully: {obj}")
+                    logger.info(f"Global devices config created successfully: {obj}")
 
 
-                        get_onboarding_token(
-                            config_data['device_id'], config_data['gateway_id'], config_data['keycloak_url'], config_data['realm_password'])
-                        
-                        with open(resources + '/devices-secret.yaml', 'r') as edit_file:
-                            edit_data = edit_file.read()
-                            body_data=yaml.safe_load(edit_data)
-                            kopf.adopt(body_data)
+                get_onboarding_token(
+                    config_data['device_id'], config_data['gateway_id'], config_data['keycloak_url'], config_data['realm_password'])
+                
+                with open(resources + '/devices-secret.yaml', 'r') as edit_file:
+                    edit_data = edit_file.read()
+                    body_data=yaml.safe_load(edit_data)
+                    kopf.adopt(body_data)
 
-                            api = kubernetes.client.CoreV1Api()
-                            obj = api.create_namespaced_secret(
-                                namespace=namespace,
-                                body=body_data
-                            )
+                    api = kubernetes.client.CoreV1Api()
+                    obj = api.create_namespaced_secret(
+                        namespace=namespace,
+                        body=body_data
+                    )
 
-                            logger.info(f"Onboarding secret created successfully: {obj}")
-                        
-                        with open('./data/device.json') as device_file:
-                            device_data = device_file.read()
-                            
-                            config_map = kubernetes.client.V1ConfigMap(
-                                api_version="v1",
-                                kind="ConfigMap",
-                                metadata=kubernetes.client.V1ObjectMeta(name=config_data['pod_name'] + "-devices-data-config"),
-                                data={"device.json": device_data}
-                            )
+                    logger.info(f"Onboarding secret created successfully: {obj}")
+                
+                with open('./data/device.json') as device_file:
+                    device_data = device_file.read()
+                    
+                    config_map = kubernetes.client.V1ConfigMap(
+                        api_version="v1",
+                        kind="ConfigMap",
+                        metadata=kubernetes.client.V1ObjectMeta(name=config_data['pod_name'] + "-devices-data-config"),
+                        data={"device.json": device_data}
+                    )
 
-                            kopf.adopt(config_map)
+                    kopf.adopt(config_map)
 
-                            api = kubernetes.client.CoreV1Api()
-                            obj = api.create_namespaced_config_map(
-                                namespace=namespace,
-                                body=config_map
-                            )
+                    api = kubernetes.client.CoreV1Api()
+                    obj = api.create_namespaced_config_map(
+                        namespace=namespace,
+                        body=config_map
+                    )
 
-                            logger.info(f"Device congfig created successfully: {obj}")
+                    logger.info(f"Device congfig created successfully: {obj}")
 
-                        with open(resources + '/deployment.yaml', 'r') as edit_file:
-                            edit_data = edit_file.read()
-                            formatted_yaml = edit_data.replace('pod_name', config_data['pod_name']).replace(
-                                    'PLACEHOLDER', PLACEHOLDER).replace('username_config', str(config_data['username_config'])).replace('password_config', str(config_data['password_config'])).replace(
-                                                    'dataservice_image_config', config_data['dataservice_image_config']).replace(
-                                                    'agentservice_image_config', config_data['agentservice_image_config'])
-                            
-                            body_data=yaml.safe_load(formatted_yaml)
-                            kopf.adopt(body_data)
+                with open(resources + '/deployment.yaml', 'r') as edit_file:
+                    edit_data = edit_file.read()
+                    formatted_yaml = edit_data.replace('pod_name', config_data['pod_name']).replace(
+                            'PLACEHOLDER', PLACEHOLDER).replace('username_config', str(config_data['username_config'])).replace('password_config', str(config_data['password_config'])).replace(
+                                            'dataservice_image_config', config_data['dataservice_image_config']).replace(
+                                            'agentservice_image_config', config_data['agentservice_image_config'])
+                    
+                    body_data=yaml.safe_load(formatted_yaml)
+                    kopf.adopt(body_data)
 
-                            api = kubernetes.client.AppsV1Api()
-                            obj = api.create_namespaced_deployment(
-                                namespace=namespace,
-                                body=body_data
-                            )
+                    api = kubernetes.client.AppsV1Api()
+                    obj = api.create_namespaced_deployment(
+                        namespace=namespace,
+                        body=body_data
+                    )
 
-                            logger.info(f"Data Service Deployment created successfully: {obj}")
-                    else:
-                        logger.info(f"Searching gateway config files")
-                else:
-                    logger.info(f"Searching gateway config files")
+                    logger.info(f"Data Service Deployment created successfully: {obj}")
         else:
             logger.info(f"No deployment queue file found in the aggregation location")
     
